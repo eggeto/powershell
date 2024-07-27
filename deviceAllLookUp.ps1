1a4518e6782a66a235d08d309b830f509c4d368e
eggeto <56840011+eggeto@users.noreply.github.com>
2024-07-27 17:34:50 +0200

    Add files via upload

---
+++ deviceAllLookUp.ps1
@@ -0,0 +1,56 @@
+<#
+Version 1
+look up device information by devicename or device ID or AAD device ID or email adres (UPN) from user
+
+need to fix the follow:
+if a user have more devices it will only take the first 
+add option if you know what kind of filter you want to use 
+error when input is aad device id > give warning
+
+"?`$filter=userPrincipalName eq '$userPrincipalName"
+#>
+
+Connect-MgGraph
+
+function DeviceAllLookUp {
+    param (
+        $information,
+        $chooseFilter
+    )
+    $addFilter = ""
+    $filterUpn = "?`$filter=userPrincipalName eq '$information'"
+    $filterDeviceName = "?`$filter=deviceName eq '$information'"
+    $pattern = "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$"
+
+    if ($information.Contains("@")){
+        $addFilter = $filterUpn
+    }
+    elseif ($information -match $pattern) {
+        $addFilter = $input1 
+    }
+    else {
+        $addFilter = $filterDeviceName
+    }
+
+    $uri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
+    $uriFilter = "$uri/$addFilter"
+
+    $responseDevice = (Invoke-MgGraphRequest -Method GET -uri $uriFilter).value 
+
+    [PSCustomObject][ordered]@{
+        intuneDeviceId      = $responseDevice.Id
+        azureAdDeviceId     = $responseDevice.azureAdDeviceId
+        deviceName          = $responseDevice.deviceName 
+        userEmail           = $responseDevice.UserPrincipalName 
+        #add what you need
+    }
+}
+
+$information = read-host "Enter DeviceName, DeviceId or UserPrincipalName"
+$test = DeviceAllLookUp -information $information
+$test.deviceName
+$test.userEmail
+$test.intuneDeviceId
+$test.azureAdDeviceId
+
+disconnect-mggraph
