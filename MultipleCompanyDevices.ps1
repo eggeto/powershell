322f07d4e13c49458d6b8f197ddd715188c4af21
eggeto <56840011+eggeto@users.noreply.github.com>
2025-06-02 13:37:56 +0200

    Update MultipleCompanyDevices.ps1
    adding function looping user group

--- MultipleCompanyDevices.ps1
+++ MultipleCompanyDevices.ps1
@@ -1,23 +1,32 @@
 <# 
     .SYNOPSIS
-    Get the Company devices from users with more then 1 device
+    Get the devices from user(s) with more then 1 device
 
     .DESCRIPTION
     For this script, you need to install the powershell mgraph module.
-    It is made for detecting users with more then 1 Company Device(s)!
+    It is made for filerting users with more then 1 Company Device(s)!
+    from a group or single user
     You can modify the script, for example more then 3 company devices or Personal devices or ...
 
     .INPUTS
-    user principal name or email
+    A group Id from a group, where the member(s) are user(s) => UsersWithMoreCompanyDevices -groupId $groupId
+    or for one user, principal name or email => CountCompanyDevices -upn $upn
 
     .OUTPUTS
-    The output is an psobject contain the user and the device names in a list
+    The output is an psobject, contains the user mail and the device names in a list
     you can modify this also to an array or ...
 
     Made by Eggeto
+
+    .logs
+    made 02/06/2025
+
+    .to do
+    ask what the user preffer, number of devices, company or personal
 #>
 
-Connect-MgGraph -Scopes User.Read.All
+Connect-MgGraph -Scopes User.Read.All,GroupMember.Read.All,Group.Read.All
+#Check if a list with devices are company or personal
 function IsCompanyDevice {
     param (
         $response    
@@ -34,7 +43,7 @@ function IsCompanyDevice {
     }  
     return $listCompanyOwnedDevices  
 }
-
+#return psobject when a user have more then 1 company device
 function CountCompanyDevices { #i want every one with mor then 1 device
     param (
         $upn
@@ -44,13 +53,13 @@ function CountCompanyDevices { #i want every one with mor then 1 device
         $response = (Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction SilentlyContinue -StatusCodeVariable "status1").value
     }
     catch {
-        Write-Host "MAYDAY for $upn"
-        return "Error details: $($_.Exception.Message)"  
+        return "MAYDAY for $upn, Error details: $($_.Exception.Message)"  
     }
     $response = @($response)
     #$arrayCompanyOwnedDevices = @{}
     $companyOwnedDevices = IsCompanyDevice -response $response
-    $count = $companyOwnedDevices.count
+
+    $count = $companyOwnedDevices.deviceName.count # count is 55?
 
     if($count -gt 1){ #change this to whatever you want
         $resultCompanyOwnedDevices = [PSCustomObject]@{
@@ -68,7 +77,31 @@ function CountCompanyDevices { #i want every one with mor then 1 device
         return $null
     } 
 }
+#looping a user group
+function UsersWithMoreCompanyDevices {
+    param (
+        $groupId
+    )
+    $uri = "https://graph.microsoft.com/v1.0/groups/$groupId/members"
+    try {
+        $response = (Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction SilentlyContinue -StatusCodeVariable "status1").value
+    }
+    catch {
+        Write-Host "MAYDAY for $upn"
+        return "Error details: $($_.Exception.Message)"  
+    }
+    $response = @($response)
+    $userFromGroup = $response.mail #$response.userPrincipalName
 
-CountCompanyDevices -upn #<the email or UPN from 1 user or if you can add a loop>
+    $listUserDevice = @()
+    foreach ($user in $userFromGroup) {
+        $outputUser = CountCompanyDevices -upn "$user"
+        $listUserDevice += $outputUser
+    }
+    $listUserDevice
+}
+
+$groupId = "YOUR GROUP ID"
+UsersWithMoreCompanyDevices -groupId $groupId
 
 DisConnect-MgGraph
