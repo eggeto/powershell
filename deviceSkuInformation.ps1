5bd868649f9ff3e37654aaee24fd5a7e3e7675b0
Eggeto <56840011+eggeto@users.noreply.github.com>
2025-07-30 14:45:52 +0200

    Update deviceSkuInformation.ps1
    update to version 1

--- deviceSkuInformation.ps1
+++ deviceSkuInformation.ps1
@@ -31,8 +31,10 @@ skuNumber legend:
 206	Windows 11 IoT Enterprise GAC	Government/China variant
 133	Windows SE	=> Lightweight Windows 11 for education / Surface Laptop
 
+when a device is intune joined, the license will become an enterprise license. Even when it is an pro license!
+
 .NOTES
-    Version:        Beta
+    Version:        1
     Author:         eggeto
     Creation Date:  2025-07-30
     Requirements:   
@@ -40,60 +42,96 @@ skuNumber legend:
     - Microsoft Graph PowerShell SDK modules
 #>
 
-
-#Connect-MgGraph
-$allWindowsDeviceInfo = @()
-$filter = "?`$select=id,deviceName,emailAddress"
-$uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices$filter"
-
-try {
-    $response = (Invoke-MgGraphRequest -uri $uri -Method Get -OutputType PSObject).value
+#paginated Graph API call => not my function
+function Get-GraphPagedResults {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Uri
+    )
+    
+    $results = @()
+    $nextLink = $Uri
+    
+    do {
+        try {
+            $response = Invoke-MgGraphRequest -Uri $nextLink -Method GET
+            if ($response.value) {
+                $results += $response.value
+            }
+            $nextLink = $response.'@odata.nextLink'
+        }
+        catch {
+            Write-Log "Error in pagination: $_"
+            break
+        }
+    } while ($nextLink)
+    
+    return $results
 }
-catch {
-    #something
-}
-foreach ($device in $response) {
-    $intuneId = $device.id
-    $filterSku = "?`$select=skuFamily,skuNumber"
-    $uriSku = "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$intuneId$filterSku"
+#Get all devices in Intune
+function AllDevices {
+    param (
+    )
+    $filter = "?`$select=id,deviceName,emailAddress"
+    $uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices$filter"
+    $response = Get-GraphPagedResults -Uri $uri
+    return $response
+    <#
     try {
-        $responseSku = (Invoke-MgGraphRequest -uri $uriSku -Method Get -OutputType PSObject)
-
-        $skuNumber = $responseSku.skuNumber
-        $windows =  $filter = switch ($skuNumber) {
-            4 { "Windows Pro" }
-            48 { "Windows Enterprise" }
-            98 { "Windows Education" }
-            100 { "Windows Home" }
-            101 { "Windows Home N" }
-            121 { "Windows Pro Education" }
-            119 { "Windows Pro for Workstations" }
-            125 { "Windows 10 IoT Enterprise 2019 LTSC" }
-            191 { "Windows 10 IoT Enterprise 2021 LTSC" }
-            205 { "Windows 11 IoT Enterprise 2021 LTSC" }
-            206 { "Windows 11 IoT Enterprise GAC" }
-            133 { "Windows SE" }
-            default { "Unknown SKU: $skuNumber" }
-        }
-        
-        $deviceInfo = [pscustomobject]@{
-            intuneId = $intuneId
-            deviceName = $device.deviceName
-            emailAddress = $device.emailAddress
-            windowsVersion = $windows
-            #skuFamily = $responseSku.skuFamily
-            #skuNumber = $responseSku.skuNumber
-        }
-        $allWindowsDeviceInfo += $deviceInfo
+         #(Invoke-MgGraphRequest -uri $nextlink -Method Get -OutputType PSObject).value
+        return $response
     }
     catch {
-        continue
+        return $($_.Exception.Message)
     }
+    #>
 }
-$allWindowsDeviceInfo = $allWindowsDeviceInfo | sort-object -Property windowsVersion 
+#add windows verion info to pstable
+function GetWindowsInfo {
+    param (
+    )
+    $allDevices = AllDevices
+    $allWindowsDeviceInfo = @()
+    foreach ($device in $allDevices) {
+        $intuneId = $device.id
+        $filterSku = "?`$select=skuFamily,skuNumber"
+        $uriSku = "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$intuneId$filterSku"
+        try {
+            $responseSku = (Invoke-MgGraphRequest -uri $uriSku -Method Get -OutputType PSObject)
 
-$allWindowsDeviceInfo
+        $skuNumber = $responseSku.skuNumber
+        $windows = switch ($skuNumber) {
+                4 { "Windows Pro" }
+                48 { "Windows Enterprise" }
+                98 { "Windows Education" }
+                100 { "Windows Home" }
+                101 { "Windows Home N" }
+                121 { "Windows Pro Education" }
+                119 { "Windows Pro for Workstations" }
+                125 { "Windows 10 IoT Enterprise 2019 LTSC" }
+                191 { "Windows 10 IoT Enterprise 2021 LTSC" }
+                205 { "Windows 11 IoT Enterprise 2021 LTSC" }
+                206 { "Windows 11 IoT Enterprise GAC" }
+                133 { "Windows SE" }
+                default { "Unknown SKU: $skuNumber" }
+            }
+            $deviceInfo = [pscustomobject]@{
+                intuneId = $intuneId
+                deviceName = $device.deviceName
+                emailAddress = $device.emailAddress
+                windowsVersion = $windows
+            }
+            $allWindowsDeviceInfo += $deviceInfo
+        }
+        catch {
+            continue
+        }
+    }
+    $allWindowsDeviceInfo = $allWindowsDeviceInfo | sort-object -Property windowsVersion 
+    return $allWindowsDeviceInfo
+}
+Connect-MgGraph
 
+GetWindowsInfo
 
-
-#disConnect-MgGraph
+disConnect-MgGraph
