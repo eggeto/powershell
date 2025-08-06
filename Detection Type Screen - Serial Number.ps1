e436a7a54b40c241928836068c2839c13130772b
Eggeto <56840011+eggeto@users.noreply.github.com>
2025-08-06 12:33:03 +0200

    Update Detection Type Screen - Serial Number.ps1
    add psobject

--- Detection Type Screen - Serial Number.ps1
+++ Detection Type Screen - Serial Number.ps1
@@ -1,20 +1,26 @@
 #Detection Type Screen + Serial Number
-$displays = Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorID
+$allDisplays = Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorID
 
-$displayInfo = foreach ($display in $displays) {
+$allConnectedScreens = @()
+foreach ($display in $allDisplays) {
     $nameChars = $display.UserFriendlyName | Where-Object { $_ -ne 0 } | ForEach-Object { [char]$_ }
     $serialChars = $display.SerialNumberID | Where-Object { $_ -ne 0 } | ForEach-Object { [char]$_ }
 
     $nameScreen = ($nameChars -join "").Trim()
     $serialScreen = ($serialChars -join "").Trim()
 
-    "$nameScreen ($serialScreen)"
+    $connectedScreen = [PSCustomObject]@{
+        nameScreen = $nameScreen
+        serialScreen = $serialScreen
+    }
+    $allConnectedScreens += $connectedScreen
 }
 
-if ($displayInfo.Count -gt 0) {
-    Write-Host "$($displayInfo -join ', ') $env:HostIP"
+if ($allConnectedScreens.Count -gt 0) {
+    Write-Host $allConnectedScreens $env:HostIP
     exit 1
 } else {
     Write-Host "no screen detected"
     exit 0
 }
+
