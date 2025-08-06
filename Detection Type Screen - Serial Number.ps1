90e343057ee06eb9d04f218f688137832376a969
Eggeto <56840011+eggeto@users.noreply.github.com>
2025-08-06 12:22:57 +0200

    Update Detection Type Screen - Serial Number.ps1
    update script

--- Detection Type Screen - Serial Number.ps1
+++ Detection Type Screen - Serial Number.ps1
@@ -1,16 +1,20 @@
 #Detection Type Screen + Serial Number
+$displays = Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorID
 
-$display = Get-CimInstance WmiMonitorID -Namespace root\wmi | 
-ForEach-Object {($_.UserFriendlyName -ne 0 | 
-    ForEach-Object {[char]$_}) -join ""; ($_.SerialNumberID -ne 0 | 
-    ForEach-Object {[char]$_}) -join ""}
+$displayInfo = foreach ($display in $displays) {
+    $nameChars = $display.UserFriendlyName | Where-Object { $_ -ne 0 } | ForEach-Object { [char]$_ }
+    $serialChars = $display.SerialNumberID | Where-Object { $_ -ne 0 } | ForEach-Object { [char]$_ }
 
+    $nameScreen = ($nameChars -join "").Trim()
+    $serialScreen = ($serialChars -join "").Trim()
 
-if ($display -gt 2){
-    Write-host $display $env:HostIP
-    exit 1
+    "$nameScreen ($serialScreen)"
 }
-else {
+
+if ($displayInfo.Count -gt 2) {
+    Write-Host "$($displayInfo -join ', ') $env:HostIP"
+    exit 1
+} else {
     Write-Host "no screen detected"
     exit 0
 }
