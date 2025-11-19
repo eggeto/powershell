70b57de429fd726d3f4c77cfc1d0d1c58ed3b359
Eggeto <56840011+eggeto@users.noreply.github.com>
2025-11-19 11:47:00 +0100

    Create LAPS password retrieval script for Azure AD
    This script retrieves the LAPS password for a user or device from Azure AD, handling multiple devices and errors.

---
+++ LapsPassword.ps1
@@ -0,0 +1,69 @@
+<#
+Get laps password from user or device
+
+a user can have multiple devices,
+but a device can have 1 laps 
+need to add for a device
+#>
+#Get Azure AD Device ID from user
+function GetAzureAdDeviceId {
+    param (
+        $upn
+    )
+    $filter = "?`$select=azureADDeviceId,emailAddress&`$filter=emailAddress eq '$upn'"
+    $uri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices$filter"
+    try {
+        $responseId = (invoke-mggraphrequest -Method GET -Uri $uri).value
+        write-host "Azure AD Device ID retrieved"
+        return $responseId.azureADDeviceId
+    }
+    catch {
+        return "MAYDAY, failed retrive azure device id: $($_.Exception.Message)"
+    }
+}
+#Get Azure AD Device id from device
+function GetAzureIdFromDevice {
+    param (
+        $deviceName
+    )
+    $filter = "?`$select=azureADDeviceId,deviceName&`$filter=deviceName eq '$deviceName'"
+    $uri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices$filter"
+    try {
+        $responseId = (invoke-mggraphrequest -Method GET -Uri $uri).value
+        write-host "Azure AD Device ID retrieved"
+        return $responseId.azureADDeviceId
+    }
+    catch {
+        return "MAYDAY, failed retrive azure device id: $($_.Exception.Message)"
+    }
+}
+#Get LAPS password from user
+function GetLapsPasswordUser {
+    param (
+        $azureId
+    )
+    $filter = "?`$select=credentials"
+    $uri = "https://graph.microsoft.com/beta/directory/deviceLocalCredentials/$azureId$filter"
+    try {
+        $response = Invoke-MgGraphRequest -Method GET -Uri $uri
+        write-host "password retrieved"
+    }
+    catch {
+        return "MAYDAY, failed retrive password: $($_.Exception.Message)" 
+    }
+#convert base64 to clear text
+    $password = [system.text.encoding]::UTF8.GetString([System.Convert]::FromBase64String($response.credentials[0].passwordBase64))
+    write-host "the LAPS password is: $password for user: $user"
+    Return $password
+}
+
+Connect-Mggraph -Scope DeviceLocalCredential.Read.All, Device.Read.All, User.Read.All
+
+$user = "USER EMAIL ADRES"
+#$device = "DEVICE NAME DISPLAYNAME INTUNE"
+
+$azureId = GetAzureAdDeviceId -upn $user
+#or $azureId = GetAzureIdFromDevice -deviceName $device
+GetLapsPasswordUser -azureId $azureId
+
+#disConnect-Mggraph
