8711856d34d9cd6e59f9d5bd3037ecb9d17fb5be
Eggeto <56840011+eggeto@users.noreply.github.com>
2026-01-26 19:06:12 +0100

    Revise comments and add script header
    Updated comments and added a header for the script.

---
+++ GetContentScript.ps1
@@ -0,0 +1,77 @@
+<#
+Get all scripts + content from intune scripts (not remediations!!!)
+
+If you want to know the content from a script in intune
+and you have the name, but don't have the original script.
+
+Thsi script will retrevie the code from that script
+
+made: 26/01/2026
+by: eggeto
+#>
+
+function GetInfoScript {
+    param (
+        $info
+    )
+    $scriptId = $info.id
+    $uri = "https://graph.microsoft.com/beta/deviceManagement/deviceManagementScripts/$scriptId"
+    
+    try {
+        $response = Invoke-MgGraphRequest -Method GET -Uri $uri
+    }
+    catch {
+        return "Can't find the script: $($_.Exception.Message)" 
+    }
+
+#decode base64 content
+    $decode = $response.scriptContent
+    $bytes  = [System.Convert]::FromBase64String($decode)
+    $text   = [System.Text.Encoding]::UTF8.GetString($bytes)
+
+    return $text
+
+}
+
+function main {
+    param (
+        
+    )
+#Get all scripts info
+    $filter = "?`$select=id,displayName,description,fileName"    
+    $uri = "https://graph.microsoft.com/beta/deviceManagement/deviceManagementScripts$filter"
+
+    try {
+        $response = (Invoke-MgGraphRequest -Method GET -Uri $uri -OutputType PSObject).value
+    }
+    catch {
+        return "No scripts found $($_.Exception.Message)" 
+    }
+    
+    write-host "All available scripts names" -ForegroundColor Green
+    write-host $response.displayName -ForegroundColor Cyan
+    write-host ""
+
+    $input = Read-Host "Type the script name, you want to see the content from:"
+    write-host ""
+    
+    foreach ($item in $response) {
+        if ($item.displayName -eq $input) {
+            $scriptName = $item.displayName
+            $scriptDescription = $item.description
+#get content from script + show it
+            $getInfo = GetInfoScript -info $item
+#write out
+            Write-Host "the name is: $scriptName" 
+            Write-Host "with description: $scriptDescription"
+            Write-Host "the content script is: $getInfo"
+        }
+    }    
+}
+
+Connect-MgGraph -Scopes DeviceManagementScripts.Read.All
+
+main
+
+#disConnect-MgGraph
+
