4a947ba580eeef4ce3fd22f5f786375e783cb2bd
Eggeto <56840011+eggeto@users.noreply.github.com>
2026-04-16 17:04:00 +0200

    Enhance device management for Intune and Sophos
    Added functions to retrieve Intune devices and Sophos devices, including pagination handling. Implemented sorting for devices not in Intune and servers inactive for over 6 months.

--- SophosEndpoints/RemovingOldDevicesSophosCentral.ps1
+++ SophosEndpoints/RemovingOldDevicesSophosCentral.ps1
@@ -13,6 +13,7 @@ the script checks the last seen date of the servers in Sophos Central.
 If a server has not been seen for 6 or more months (can be modified), it is removed from Sophos Central.
 
 !!CHECK THE OUTPUT CAREFULLY BEFORE REMOVING THE COMMAND FROM THE FUNCTION DeleteDevicesSophos, ONCE IT RUNS IT CAN'T BE UNDONE !!
+don't forget to fill in the clientid & clientsecret!!
 
 .EXAMPLE
 => .output
@@ -45,3 +46,229 @@ hostname         lastSeenAt          type     os.name
     - Microsoft Graph PowerShell SDK modules
     - Sophos Central API credentials (client ID and client secret)
 #>
+###############  Intune  ###############
+#paginated Graph API call for intune devices
+function Get-GraphPagedResults {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Uri
+    )
+    
+    $results = @()
+    $nextLink = $Uri
+    
+    do {
+        try {
+            $response = Invoke-MgGraphRequest -Uri $nextLink -Method GET
+            if ($response.value) {
+                $results += $response.value
+            }
+            $nextLink = $response.'@odata.nextLink'
+        }
+        catch {
+            Write-Log "Error in pagination: $_"
+            break
+        }
+    } while ($nextLink)
+    
+    return $results
+}
+#Get all Intune Devices by devicename
+function GetAllIntuneDevices {
+    param (
+    )
+    $filter = "?`$select=DeviceName"
+    $uri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices$filter"
+
+    try {
+        $intuneDevicesResponse = Get-GraphPagedResults -Uri $uri
+        write-host "Got Intune devices" -ForegroundColor Green 
+        return $intuneDevicesResponse
+    } catch {
+        write-host "failed getting Intune Devices: $_" -ForegroundColor Red
+        return $Error
+    }
+}
+
+###############  Sophos  ###############
+#Create token
+function GetSophosToken {
+    param(
+        $clientId,
+        $clientSecret
+    )
+#Step 1 -> Get Token
+    try {
+        $tokenResponse = Invoke-RestMethod -Method POST -Uri "https://id.sophos.com/api/v2/oauth2/token" -ContentType "application/x-www-form-urlencoded" -Body "grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret&scope=token"
+        write-host "Got Token" -ForegroundColor Green
+    }
+    catch {
+        write-host "failed getting token: $_" -ForegroundColor Red
+        return $Error
+    }
+    $accessToken = $tokenResponse.access_token
+
+#Step 2 -> Get tenantId (tenant + region) 
+    try {
+        $tenantResponse = Invoke-RestMethod -Method GET -Uri "https://api.central.sophos.com/whoami/v1" -Headers @{ Authorization = "Bearer $accessToken" }
+        write-host "Got tenant info" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "failed getting tenant info: $_" -ForegroundColor Red
+        return $Error
+    }
+
+    #$tenantId = $tenantResponse.id
+    #$baseUrl  = $tenantResponse.apiHosts.dataRegion
+
+    $tenantInfo = @{
+        accessToken = $accessToken
+        tenantId = $tenantResponse.id
+        baseUrl  = $tenantResponse.apiHosts.dataRegion
+    }
+
+    return $tenantInfo
+}
+#Will get all endpoints and servers!
+function GetAllSophosDevices {
+    param (
+        $tenantInfo
+    )
+    $accessToken = $tenantInfo.accessToken
+    $tenantId = $tenantInfo.tenantId
+#step 3: Get all Sophos Devices with pagination
+    $headers = @{
+        Authorization = "Bearer $accessToken"
+        "X-Tenant-ID" = $tenantId
+    }
+
+    $allDevices = @()
+    $nextKey    = $null
+    $page       = 1
+    $baseUrl     = $tenantInfo.baseUrl
+    do {
+#Build URL  — nextKey added from page 2
+        $url = "$baseUrl/endpoint/v1/endpoints?sort=lastSeenAt:asc&pageSize=100"
+        if ($nextKey) {
+            $url += "&pageFromKey=$nextKey"
+        }
+
+        #Write-Host "Collect page $page " -ForegroundColor Cyan
+
+        $response = Invoke-RestMethod -Method GET -Uri $url -Headers $headers
+
+        $allDevices += $response.items
+        $nextKey     = $response.pages.nextKey
+        $page++
+
+    } while ($nextKey)
+
+    #Write-Host "Total Sophos devices: $($allDevices.Count)" -ForegroundColor Green
+    return $allDevices
+}
+#Remove old devices from Sophos Central (not in Intune and not active for + 6 months)
+function DeleteDevicesSophos {
+    param (
+        $tenantInfo,
+        $sortedDevices,
+        $sortedServers
+    )
+#Merge the 2 lists
+    $devicesToBeRemoved = $sortedDevices + $sortedServers
+#connect to Sophos again
+    $accessToken = $tenantInfo.accessToken
+    $tenantId = $tenantInfo.tenantId
+    $headers = @{
+        Authorization = "Bearer $accessToken"
+        "X-Tenant-ID" = $tenantId
+    }
+    $baseUrl= $tenantInfo.baseUrl
+
+#removing the devices
+    foreach ($device in $devicesToBeRemoved) {
+
+        try {
+            write-host "Removing device: $($device.hostname)" -ForegroundColor Yellow
+#if you are pretty sure, you can command this
+            Start-Sleep -Seconds 10
+            Invoke-RestMethod -Method DELETE -Uri "$baseUrl/endpoint/v1/endpoints/$($device.id)" -Headers $headers | Out-Null
+        }
+        catch {
+            Write-Warning "error removing device:  $($device.hostname): $_"
+        }
+    }
+}
+###############  Filtering/Sorting  ###############
+#Sort clients that are not in Intune (by devicename/hostname)
+function SortDevices {
+    param (
+        $listSophosDevices,
+        $listIntuneDevices
+    )
+    $allNotMatched = [System.Collections.Generic.List[object]]::new()
+    foreach ($sophosDevice in $listSophosDevices) {
+        $found = $false
+        foreach  ($intuneDevice  in $listIntuneDevices) {
+            $sophosHostname = $sophosDevice.hostname
+            $intuneDeviceName = $intuneDevice.DeviceName
+            if ($sophosHostname -eq $intuneDeviceName) {
+                $found = $true
+                break
+            }
+        }
+        $deviceType = $sophosDevice.type
+        if (-not $found -and $deviceType -ne "server") {
+            #Write-Host "Not found: $sophosDevice"
+            $allNotMatched.add($sophosDevice)
+        } 
+<#
+        else {
+            #Write-Host "Found: $sophosDevice"
+        }   
+#>
+    }
+    return $allNotMatched
+}
+#Sort servers that are not online for + 6 months 
+function SortServers {
+    param (
+        $listSophosDevices
+    )
+    #step 4: Get all non active devices
+    $targetDate = (Get-Date).AddMonths(-6)
+
+    $oldServers = [System.Collections.Generic.List[object]]::new()
+    foreach ($device in $listSophosDevices) {
+        $deviceDate = [datetime]$device.lastSeenAt #lastseen => it is a string!! 
+        $deviceType = $device.type
+        if ($deviceType -eq "server" -and $deviceDate -lt $targetDate) {
+            $oldServers.Add($device) #start using method add() instead of += !!!!
+        
+        }
+    }
+
+# Tonen in console
+    #write-host $oldDevices 
+    return $oldServers
+}
+
+Connect-MgGraph
+$allIntuneDevices = GetAllIntuneDevices
+
+#Config => in Sophos Central -> Global Settings -> API Credentials Management
+$clientId     = "YOUR CLIENT ID"
+$clientSecret = "TOYR CLIENT SECRET"
+
+$tenantInfo = GetSophosToken -clientId $clientId -clientSecret $clientSecret
+$allSophosDevices = GetAllSophosDevices -tenantInfo $tenantInfo
+
+write-host "ARE YOU SURE YOU WANT TO REMOVE FOLLOWING DEVICES FROM SOPHOS CENTRAL: " -ForegroundColor Red
+write-host "Devices not in Intune: " -ForegroundColor Yellow
+$sortedDevices = SortDevices -listSophosDevices $allSophosDevices -listIntuneDevices $allIntuneDevices
+$sortedDevices | Select-Object hostname,lastSeenAt,type,os.name | Format-Table -AutoSize
+write-host "Servers not online for + 6 months: " -ForegroundColor Yellow
+$sortedServers = SortServers -listSophosDevices $allSophosDevices 
+$sortedServers | Select-Object hostname,lastSeenAt,type,os.name | Format-Table -AutoSize
+
+#made it a command for your own safety :-), removing the command at your own risk
+#DeleteDevicesSophos -tenantInfo $tenantInfo -sortedDevices $sortedDevices -sortedServers $sortedServers
