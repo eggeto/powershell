acc72bbdc9d64cb2e0c517007f0fc33990b1d5c7
Eggeto <56840011+eggeto@users.noreply.github.com>
2026-08-10 11:21:35 +0200

    Enhance Xbox app detection script output
    Updated detection script to output apps marked for removal.

--- RemediationScripts/RemoveXboxApps/detection.ps1
+++ RemediationScripts/RemoveXboxApps/detection.ps1
@@ -4,9 +4,10 @@ if ($xboxOnit){
     write-host "xbox apps are installed"
     foreach ($app in $xboxOnIt) {
         if ($app.NonRemovable) {
-            Write-Output "Skipping $($app.Name) - marked NonRemovable"
+            Write-Output "Skip $($app.Name) - marked NonRemovable"
             continue
         }
+        Write-Output "Got $($app.Name) - marked for removal"
     }
     exit 1
 }
