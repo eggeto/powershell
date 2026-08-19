d8f43e927ecd04aff6963fc23f1245c3a84119f2
Eggeto <56840011+eggeto@users.noreply.github.com>
2026-08-19 16:45:51 +0200

    Create enableSecureBoot.ps1 for Intune deployment
    Added a PowerShell script for enabling Secure Boot via Intune.

---
+++ Intune Complaince (remediation) scripts/Remediation/Enable Secure Boot/enableSecureBoot.ps1
@@ -0,0 +1,10 @@
+<#
+I have tried various ways of installing a PowerShell module via App/Script in Intune.
+
+The method involving downloading the module via the PowerShell script failed every time.
+I have abandoned that option as I have already spent too much time on it, without success.
+
+The simpler option, where you download the module and package it as a Win32 app, worked straight away.
+The PowerShell script below was created for this purpose.
+You can also find the IntuneWin file in this folder.
+#>
